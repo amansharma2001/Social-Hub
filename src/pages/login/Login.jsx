12a65b26db31b1ea95810f9c750ce43e3c24f2f4
amansharma2001 <89450216+amansharma2001@users.noreply.github.com>
2022-08-14 10:52:19 +0530

    Social Media React App

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import "./login.css";
+
+
+export default function Login() {
+  return (
+    <div className="login">
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">SocialHub</h3>
+                <span className="loginDesc">
+                    Connect with friends and the world around you on SocialHub.
+                </span>
+            </div>
+            <div className="loginRight">
+                <div className="loginBox">
+                        <input placeholder="Email" type="text" className="loginInput" />
+                        <input placeholder="Password" type="text" className="loginInput" />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password ?</span>
+                        <button className="loginRegisterButton">Create a New Account</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,42 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Leftbar from "../../components/leftbar/Leftbar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile() {
+  return (
+    <>
+    
+        <Topbar/>
+        <div className="profile">
+          <Leftbar/>
+          <div className="profileRight">
+            <div className="profileRightTop">
+                <div className="profileCover">
+                    <img 
+                        className="profileCoverImg"
+                        src="assests/post/3.jpeg" 
+                        alt="" 
+                        />
+                    <img 
+                        className="profileUserImg" 
+                        src="assests/person/7.jpeg" 
+                        alt="" 
+                        />
+                </div>
+                <div className="profileInfo">
+                    <h4 className="profileInfoName">Aman Sharma</h4>
+                    <span className="profileInfoDesc">Hello my Friends</span>
+                </div>
+            </div>
+            <div className="profileRightBottom">
+                <Feed/>
+                <Rightbar profile/>
+            </div>
+          </div>
+          
+        </div>
+    </>
+  )
+}
